0a3bddc64e559b25f19ab7894f6c12ffaed7b5ac
Vincent Deville <vincent.deville@epita.fr>
2016-10-22 14:43:50 +0200

    image_to_matrix.h fonctionne

--- src/image_to_matrix.c
+++ src/image_to_matrix.c
@@ -1,13 +1,18 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 double * pixbuf_to_matrix_grey(GdkPixbuf *pixbuf,double **matrix_end, int *pointer_width, int *pointer_height)
 {
 	guchar *pixel=NULL;
 	gint channel=0;
 	gint width=0;
-	if (!pixbuf) return FALSE;
+	if (!pixbuf)
+	{
+		perror("Erreur fonction pixbuf_to_matrix_grey no pixbuf");
+		return NULL;
+	}
 	pixel=gdk_pixbuf_get_pixels(pixbuf);
 	channel=gdk_pixbuf_get_n_channels(pixbuf);
 	width=gdk_pixbuf_get_width(pixbuf);
@@ -30,36 +35,14 @@ double * pixbuf_to_matrix_grey(GdkPixbuf *pixbuf,double **matrix_end, int *point
 	*matrix_end = matrix + width*height;
 	return matrix;
 }
-double * file_to_matrix_grey(const gchar *filename, double **matrix_end, int *pointer_width, int *pointer_height)
+double * file_to_matrix_grey(const char *filename, double **matrix_end, int *pointer_width, int *pointer_height)
 {	
 	GError *error = NULL;
-        GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file(filename, &error); 
-	guchar *pixel=NULL;
-	gint channel=0;
-	gint width=0;
-	if (!pixbuf) return FALSE;
-	pixel=gdk_pixbuf_get_pixels(pixbuf);
-	channel=gdk_pixbuf_get_n_channels(pixbuf);
-	width=gdk_pixbuf_get_width(pixbuf);
-	gint height = gdk_pixbuf_get_height(pixbuf);
-	*pointer_width = width;
-	*pointer_height = height;
-	guchar red, green, blue;
-	double *matrix = malloc(sizeof(double) * width * height);
-	for (gint y = 0; y<height; y++)
-	{
-		for (gint x = 0; x<width; x++)
-		{
-			red   = pixel[(x*channel)+(y*width*channel)];
-			green = pixel[(x*channel)+(y*width*channel)+1];
-			blue  = pixel[(x*channel)+(y*width*channel)+2];
-			*(matrix + x + y*width) = (red+green+blue); 
-			*(matrix + x + y*width) = *(matrix + x + y*width)/765;//considering that the max value of each colors is 255
-		}
-	}
-	*matrix_end = matrix + width*height;
-	return matrix;
+        GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file(filename, &error);
+	return pixbuf_to_matrix_grey(pixbuf, matrix_end, pointer_width, pointer_height);
 }
+
+
 /*double * image_to_matrix_colors(GdkPixbuf *pixbuf) 	//not used and not tested
 {
 	guchar *pixel=NULL;
@@ -83,8 +66,8 @@ double * file_to_matrix_grey(const gchar *filename, double **matrix_end, int *po
 	}
 	return matrix;
 }*/
-
-/*int main(int argc, char *argv[])	//test the image_to_matrix_grey
+/*
+int main(int argc, char *argv[])	//test the image_to_matrix_grey
 {
 	double * matrix_end;
 	int width, height;
--- src/image_to_matrix.h
+++ src/image_to_matrix.h
@@ -1,15 +1,16 @@
 #ifndef IMAGE_TO_MATRIX_H
-#define FEED_FORWARD_H
+#define IMAGE_TO_MATRIX_H
 
 #include "image_to_matrix.h"
+#include <gtk/gtk.h>
 
 // input GdkPixbuf *pixbuf, double **matrix_end, int *pointer_width, int *pointer_height
 // return an double array with grey_pixel vals , and the pointers vals
-double * pixbuf_to_matrix(GdkPixbuf *pixbuf, double **matrix_endint, *pointer_width, int *pointer_height );
+double *pixbuf_to_matrix_grey(GdkPixbuf *pixbuf, double **matrix_end, int *pointer_width, int *pointer_height );
 
 // input file path const gchar *filename, double **matrix_end, int *pointer_width, int *pointer_height 
 // return an double array with grey_pixel vals, and the pointers vals
-double * file_to_matrix(GdkPixbuf *pixbuf, double **matrix_endint, *pointer_width, int *pointer_height );
+double *file_to_matrix_grey(const char *filename, double **matrix_end, int *pointer_width, int *pointer_height );
 
 
 #endif
