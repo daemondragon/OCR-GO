1f42cee88ffe28b3a0090fa6472ccf75d8be4d74
boulic_j <boulic_j@epita.fr>
2016-10-24 09:15:04 +0200

    fonction de decoupage horrizontal terminé

--- src/decoupage.c
+++ src/decoupage.c
@@ -8,8 +8,37 @@ int decoupage (void){
 return 0;
 }
 
-char black_line() {
-	
-	
-	return 0;
+char black_line(double *matrix,int l,int seuil) {
+	int dots=0;
+	double *temp = matrix;	
+	for(int i=l;i>0;--i){
+		if (*temp){
+			dots++;
+			if(dots>=seuil){
+				return 1
+			}
+		}
+		temp++;
 	}
+	return 0;
+}
+
+double* search_white(double *matrix,double *start, int l,int seuil){
+	double *end_zone = matrix;
+	while(black_line(end_zone,l,seuil)&& end_zone>start+1){
+		end_zone-=l;
+		}	
+ 	return end_zone;
+} 
+
+double* search_black(double *matrix,double *start, int l,int seuil){
+        double *end_zone = matrix;
+        while(!((black_line(end_zone,l,seuil)))&& end_zone>start+1){
+                end_zone-=l;
+                } 
+        return end_zone;
+}
+
+
+
+
