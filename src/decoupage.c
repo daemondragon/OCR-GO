234155f64432e0d43219e7667e3dd25b03a19490
boulic_j <boulic_j@epita.fr>
2016-10-26 22:55:00 +0200

    un commit qui compilenomenclature ameliorer manque le v_cutting

--- src/decoupage.c
+++ src/decoupage.c
@@ -3,19 +3,18 @@
 #include <stdio.h>
 #include "info_word.h"
 
-int decoupage (void){
-
-return 0;
-}
-
-char black_line(double *matrix,int l,int seuil) {
+char black_line(double *matrix,int l,int seuil)
+{
 	int dots=0;
 	double *temp = matrix;	
-	for(int i=l;i>0;--i){
-		if (*temp){
+	for(int i=l;i>0;--i)
+	{
+		if (*temp>0.90)
+		{
 			dots++;
-			if(dots>=seuil){
-				return 1
+			if(dots>=seuil)
+			{
+				return 1;
 			}
 		}
 		temp++;
@@ -24,35 +23,43 @@ char black_line(double *matrix,int l,int seuil) {
 }
 
 double* h_search_white(double *matrix,double *start, int l,int seuil){
-	double *end_zone = matrix;
-	while(black_line(end_zone,l,seuil)&& end_zone>start+1){
+	double *end_zone = start;
+	while(black_line(end_zone,l,seuil)&& end_zone>matrix+1){
 		end_zone-=l;
-		}	
- 	return end_zone;
+	}	
+	return end_zone;
 } 
 
 double* h_search_black(double *matrix,double *start, int l,int seuil){
-        double *end_zone = matrix;
-        while(!((black_line(end_zone,l,seuil)))&& end_zone>start+1){
-                end_zone-=l;
-                } 
-        return end_zone;
+	double *end_zone = start;
+	while(!((black_line(end_zone,l,seuil)))&& end_zone>matrix+1){
+		end_zone-=l;
+	} 
+	return end_zone;
 }
 
-W_list* cutting(double *matrix,int l, int seuil){
-	double *cursor= matrix + l - 1;
-	double *word_start;
-	double *word_end;
-	float	word_space=0;
+
+void v_cutting(double *band_start, double *band_end,int l,W_list *word_list,float *word_space,float *word_count )
+{
+
+}
+
+
+
+W_list* cutting(double *matrix,int size,int l, int seuil){
+	double *cursor= matrix + size - 1;
+	float	word_space =0;
+	float	word_count =0;
 	double *band_start;
 	double *band_end;
-	W_list *word_list
-	while (cursor >= matrix){
-		band_start = h_search_black(cursor-(double)l, matrix,l,seuil);
+	W_list *word_list=WL_init();
+	while(cursor >= matrix){
+		band_start = h_search_black(matrix,cursor+l-1,l,seuil);
 		cursor=band_start;
-		band_end = h_search_white(cursor,matrix,l,seuil);
-		 
-		
-		}
-
+		band_end = h_search_white(matrix,cursor+l-1,l,seuil);
+		cursor=band_end;
+		v_cutting(band_start,band_end,l,word_list,&word_space,&word_count);
+	}
+	return word_list;
+}
 
