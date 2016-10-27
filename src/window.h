d395fb42a53c8115ca28376b522868f0485731c4
beuvry_j <jeremy.beuvry@epita.fr>
2016-10-27 08:34:49 +0200

    window can be run with ./prog (no more separate main)

---
+++ src/window.h
@@ -0,0 +1,12 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <stdlib.h>
+#include <gtk/gtk.h>
+
+int     run_window(int argc, char **argv);
+void    on_quitter_btn(GtkWidget *pBtn, gpointer data);
+void    creer_file_selection();
+void    recuperer_chemin(GtkWidget *bouton, GtkWidget *file_selection);
+
+#endif
