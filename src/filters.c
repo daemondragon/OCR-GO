c9e2b06d5ebf35e8ac960be1a417a6e53bceedf9
Vincent Deville <vincent.deville@epita.fr>
2016-10-30 21:51:17 +0100

    filters test is working

--- src/filters.c
+++ src/filters.c
@@ -99,5 +99,18 @@ GtkWidget * file_to_image_bin(const char *filename)
 }
 void test_filters ()
 {
-	file_to_image_bin("test");
+	GtkWidget *image = file_to_image_bin("image_test/binarize.png");
+	int argc = 0;
+	char **argv = NULL;
+	gtk_init(&argc, &argv);
+	GtkWidget *Window;
+	Window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+	gtk_container_set_border_width(GTK_CONTAINER(Window),5);
+	gtk_window_set_default_size(GTK_WINDOW(Window),600,600);
+	gtk_window_set_title(GTK_WINDOW(Window),"OCR TEST IMAGE");
+	g_signal_connect(G_OBJECT(Window),"destroy", G_CALLBACK(gtk_main_quit), NULL);
+	gtk_container_add(GTK_CONTAINER(Window), image);
+	gtk_widget_show_all(Window);
+	gtk_main();
+
 }
