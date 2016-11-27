800cf1ce8e8efebe38359d8b0d7670f0f8c9f0c0
Vincent Deville <vincent.deville@epita.fr>
2016-11-27 16:25:14 +0100

    rotation added comments and improvement

--- src/rotation.h
+++ src/rotation.h
@@ -5,7 +5,8 @@
 
 
 // double matrix is the rotated matrix
-// int *width and int *height are the dimensions of the rotated matrix
+// int *width and int *height are the dimensions of the input matrix and
+// become rotated matrix dimensions
 // double angle is the angle in degrees between 0 and 90 or 270 and 360
 double *rotate (double *matrix, int *width, int *height, double angle);
 
